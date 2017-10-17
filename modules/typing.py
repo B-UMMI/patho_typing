f79495404810176944b5a2021a5ae4981471e2fb
Miguel Machado <miguelpmachado@users.noreply.github.com>
2017-10-17 14:44:45 +0100

    Add extended report

--- modules/typing.py
+++ modules/typing.py
@@ -18,6 +18,7 @@ def possible_types(data_by_gene, typing_rules_file, min_gene_coverage, min_gene_
     data_by_gene = simplify_data_by_gene(data_by_gene)
 
     possible_pathotypes = []
+    genes_present = []
     with open(typing_rules_file, 'rtU') as reader:
         genes = []
         for line in reader:
@@ -36,6 +37,7 @@ def possible_types(data_by_gene, typing_rules_file, min_gene_coverage, min_gene_
                         else:
                             if data_by_gene[genes[x]]['gene_coverage'] >= min_gene_coverage and data_by_gene[genes[x]]['gene_identity'] >= min_gene_identity and data_by_gene[genes[x]]['gene_depth'] >= min_gene_depth:
                                 gene_present = True
+                                genes_present.append(genes[x])
                             else:
                                 gene_present = False
 
@@ -45,7 +47,7 @@ def possible_types(data_by_gene, typing_rules_file, min_gene_coverage, min_gene_
                                 congruence.append(False)
                     if all(congruence):
                         possible_pathotypes.append(line[0])
-    return possible_pathotypes
+    return possible_pathotypes, list(set(genes_present))
 
 
 module_timer = functools.partial(utils.timer, name='Module Typing')
@@ -53,14 +55,20 @@ module_timer = functools.partial(utils.timer, name='Module Typing')
 
 @module_timer
 def typing(data_by_gene, typing_rules_file, min_gene_coverage, min_gene_identity, min_gene_depth, outdir):
-    possible_pathotypes = possible_types(data_by_gene, typing_rules_file, min_gene_coverage, min_gene_identity, min_gene_depth)
-    with open(os.path.join(outdir, 'patho_typing.report.txt'), 'wt') as writer:
-        if len(possible_pathotypes) > 0:
-            writer.write('\n'.join(possible_pathotypes) + '\n')
-            print '\n' + 'Pathotypes found:' + '\n'
-            print '\n'.join(possible_pathotypes) + '\n'
-        else:
-            writer.write('NA' + '\n')
-            print '\n' + 'It was not possible to identify any possible pathotype match' + '\n'
+    possible_pathotypes, genes_present = possible_types(data_by_gene, typing_rules_file, min_gene_coverage, min_gene_identity, min_gene_depth)
+    with open(os.path.join(outdir, 'patho_typing.report.txt'), 'wt') as writer_report:
+        with open(os.path.join(outdir, 'patho_typing.extended_report.txt'), 'wt') as writer_extended_report:
+            writer_extended_report.write('#Pathotypes_found' + '\n')
+            if len(possible_pathotypes) > 0:
+                writer_report.write('\n'.join(possible_pathotypes) + '\n')
+                writer_extended_report.write('\n'.join(possible_pathotypes) + '\n')
+                print '\n' + 'Pathotypes found:' + '\n'
+                print '\n'.join(possible_pathotypes) + '\n'
+            else:
+                writer_report.write('NA' + '\n')
+                writer_extended_report.write('NA' + '\n')
+                print '\n' + 'It was not possible to identify any possible pathotype match' + '\n'
+            writer_extended_report.write('\n' + '#Genes_present' + '\n')
+            writer_extended_report.write('\n'.join(genes_present) + '\n')
 
     return None, None
